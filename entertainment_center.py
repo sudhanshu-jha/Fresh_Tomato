abb827b18fae0e75b31d8d2ebe8410b9380a4a95
sudhanshu-jha <jha.sudhanshu1991@gmail.com>
2018-04-11 14:14:07 +0530

    updated links

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,12 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Up!",
+Up= media.Movie("Up!",
                   "Storyline",
                   "https://upload.wikimedia.org/wikipedia/en/0/05/Up_(2009_film).jpg",
                   "https://www.youtube.com/watch?v=pkqzFUhGPJg")
 
-avatar = media.Movie("GodFather",
+GodFather = media.Movie("GodFather",
                      "Storyline",
                      "http://vignette1.wikia.nocookie.net/godfather/images/2/2c/The_Godfather_Game.jpeg/revision/latest?cb=20131009061406",
                      "https://www.youtube.com/watch?v=sY1S34973zA")
@@ -16,22 +16,22 @@ silicon_valley = media.Movie("Pirates of Silicon Valley",
                              "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Movieposterposv.jpg/250px-Movieposterposv.jpg",
                              "https://www.youtube.com/watch?v=lEyrivrjAuU")
 
-ratatouille = media.Movie("Scarface",
+Scarface = media.Movie("Scarface",
                              "Storyline",
                              "https://upload.wikimedia.org/wikipedia/en/f/f8/ScarfaceMPR.jpg",
-                             "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+                             "https://www.youtube.com/watch?v=oSMOEKEcjqk")
 
-midnight_in_paris = media.Movie("Shawshank Redemption",
+ShawshankRedemption = media.Movie("Shawshank Redemption",
                              "Storyline",
                              "http://vignette2.wikia.nocookie.net/filmguide/images/c/cb/The_Shawshank_Redemption.jpg/revision/latest?cb=20120122172923",
-                             "https://www.youtube.com/watch?v=atLg2wQQxvU")
+                             "https://www.youtube.com/watch?v=K_tLp7T6U1c")
 
-hunger_games = media.Movie("Blade Runner",
+BladeRunner = media.Movie("Blade Runner",
                              "Storyline",
                              "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
-                             "https://www.youtube.com/watch?v=PbA63a7Hb0")
+                             "https://www.youtube.com/watch?v=gCcx85zbxz4")
 
 
-movies = [toy_story, avatar, silicon_valley, ratatouille, midnight_in_paris, hunger_games]
+movies = [Up, GodFather, silicon_valley, Scarface, ShawshankRedemption, BladeRunner]
 
 fresh_tomatoes.open_movies_page(movies)
