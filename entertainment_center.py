2fef5007ffe292b4f3fd7d8f460b30bfc350f313
Sudhanshu Jha <sudhanshu@bigdrop.io>
2019-01-14 14:58:20 +0530

    codestyle update

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,35 +1,47 @@
 import media
 import fresh_tomatoes
 
-Up= media.Movie("Up!",
-                  "Storyline",
-                  "https://upload.wikimedia.org/wikipedia/en/0/05/Up_(2009_film).jpg",
-                  "https://www.youtube.com/watch?v=pkqzFUhGPJg")
+Up = media.Movie(
+    "Up!",
+    "Storyline",
+    "https://upload.wikimedia.org/wikipedia/en/0/05/Up_(2009_film).jpg",
+    "https://www.youtube.com/watch?v=pkqzFUhGPJg",
+)
 
-GodFather = media.Movie("GodFather",
-                     "Storyline",
-                     "http://vignette1.wikia.nocookie.net/godfather/images/2/2c/The_Godfather_Game.jpeg/revision/latest?cb=20131009061406",
-                     "https://www.youtube.com/watch?v=sY1S34973zA")
+GodFather = media.Movie(
+    "GodFather",
+    "Storyline",
+    "http://vignette1.wikia.nocookie.net/godfather/images/2/2c/The_Godfather_Game.jpeg/revision/latest?cb=20131009061406",
+    "https://www.youtube.com/watch?v=sY1S34973zA",
+)
 
-silicon_valley = media.Movie("Pirates of Silicon Valley",
-                             "A biographical look at the men who made the world of technology what it is today",
-                             "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Movieposterposv.jpg/250px-Movieposterposv.jpg",
-                             "https://www.youtube.com/watch?v=lEyrivrjAuU")
+silicon_valley = media.Movie(
+    "Pirates of Silicon Valley",
+    "A biographical look at the men who made the world of technology what it is today",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Movieposterposv.jpg/250px-Movieposterposv.jpg",
+    "https://www.youtube.com/watch?v=lEyrivrjAuU",
+)
 
-Scarface = media.Movie("Scarface",
-                             "Storyline",
-                             "https://upload.wikimedia.org/wikipedia/en/f/f8/ScarfaceMPR.jpg",
-                             "https://www.youtube.com/watch?v=oSMOEKEcjqk")
+Scarface = media.Movie(
+    "Scarface",
+    "Storyline",
+    "https://upload.wikimedia.org/wikipedia/en/f/f8/ScarfaceMPR.jpg",
+    "https://www.youtube.com/watch?v=oSMOEKEcjqk",
+)
 
-ShawshankRedemption = media.Movie("Shawshank Redemption",
-                             "Storyline",
-                             "http://vignette2.wikia.nocookie.net/filmguide/images/c/cb/The_Shawshank_Redemption.jpg/revision/latest?cb=20120122172923",
-                             "https://www.youtube.com/watch?v=K_tLp7T6U1c")
+ShawshankRedemption = media.Movie(
+    "Shawshank Redemption",
+    "Storyline",
+    "http://vignette2.wikia.nocookie.net/filmguide/images/c/cb/The_Shawshank_Redemption.jpg/revision/latest?cb=20120122172923",
+    "https://www.youtube.com/watch?v=K_tLp7T6U1c",
+)
 
-BladeRunner = media.Movie("Blade Runner",
-                             "Storyline",
-                             "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
-                             "https://www.youtube.com/watch?v=gCcx85zbxz4")
+BladeRunner = media.Movie(
+    "Blade Runner",
+    "Storyline",
+    "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
+    "https://www.youtube.com/watch?v=gCcx85zbxz4",
+)
 
 
 movies = [Up, GodFather, silicon_valley, Scarface, ShawshankRedemption, BladeRunner]
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -4,7 +4,7 @@ import re
 
 
 # Styles and scripting for the page
-main_page_head = '''
+main_page_head = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -83,11 +83,11 @@ main_page_head = '''
         });
     </script>
 </head>
-'''
+"""
 
 
 # The main page layout and title bar
-main_page_content = '''
+main_page_content = """
   <body>
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
@@ -117,46 +117,46 @@ main_page_content = '''
     </div>
   </body>
 </html>
-'''
+"""
 
 
 # A single movie entry html template
-movie_tile_content = '''
+movie_tile_content = """
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
 </div>
-'''
+"""
 
 
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
-    content = ''
+    content = ""
     for movie in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = re.search(r"(?<=v=)[^&#]+", movie.trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
+            r"(?<=be/)[^&#]+", movie.trailer_youtube_url
+        )
+        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
         )
     return content
 
 
 def open_movies_page(movies):
     # Create or overwrite the output file
-    output_file = open('index.html', 'w')
+    output_file = open("index.html", "w")
 
     # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+        movie_tiles=create_movie_tiles_content(movies)
+    )
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
@@ -164,4 +164,4 @@ def open_movies_page(movies):
 
     # open the output file in the browser (in a new tab, if possible)
     url = os.path.abspath(output_file.name)
-    webbrowser.open('file://' + url, new=2)
+    webbrowser.open("file://" + url, new=2)
